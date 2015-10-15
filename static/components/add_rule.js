d7dfb31a33435e8ab536f1cba5d2e234466b03dc
Mark Mulder <markmulder@gmail.com>
2015-10-15 18:49:14 +0200

    Use buttons not a tags

--- static/components/add_rule.js
+++ static/components/add_rule.js
@@ -4,12 +4,12 @@ var AddRule = React.createClass({
   render: function() {
     const { actions } = this.props;
     return (
-      <a href='#' onClick={e => {
+      <button href='#' onClick={e => {
         e.preventDefault();
         actions.addRule();
       }}>
         Add rule
-      </a>
+      </button>
     );
   }
 });
