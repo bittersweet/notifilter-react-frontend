6eb2abaf5ceefbf91af8a4df714d6420d4a64892
Mark Mulder <markmulder@gmail.com>
2015-10-15 13:11:24 +0200

    Split up to separate component

--- static/app.js
+++ static/app.js
@@ -1,67 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var RuleForm = React.createClass({
-  getInitialState: function() {
-    return {
-      key: '',
-      type: '',
-      setting: '',
-      value: '',
-    };
-  },
-
-  updateRule: function(state) {
-    var json = JSON.stringify(state);
-    this.refs.rulesOutput.value = json;
-  },
-
-  handleChange: function(event) {
-    var newState = {
-      key: this.refs.key.value,
-      type: this.refs.type.value,
-      setting: this.refs.setting.value,
-      value: this.refs.value.value,
-    };
-    this.setState(newState);
-    this.updateRule(newState);
-  },
-
-  render: function() {
-    var key = this.state.key;
-    var type = this.state.type
-    var setting = this.state.setting;
-    var value = this.state.value;
-
-    return (
-      <div>
-        <textarea ref="rulesOutput" rows="10" cols="60" />
-        <br />
-        <select ref="key" value={key} onChange={this.handleChange}>
-          <option value="active">active</option>
-          <option value="user_role">user_role</option>
-        </select>
-        <br />
-        <select ref="type" value={type} onChange={this.handleChange}>
-          <option value="boolean">boolean</option>
-          <option value="string">string</option>
-          <option value="number">number</option>
-        </select>
-        <br />
-        <select ref="setting" value={setting} onChange={this.handleChange}>
-          <option value=""></option>
-          <option value="eq">eq</option>
-          <option value="gt">gt</option>
-          <option value="lt">lt</option>
-          <option value="noteq">noteq</option>
-        </select>
-        <br />
-        <input type="text" ref="value" value={value} onChange={this.handleChange} placeholder="Value" />
-        <br />
-      </div>
-    );
-  }
-});
+import RuleForm from './components/rule_form';
 
 var App = React.createClass({
   render: function() {
