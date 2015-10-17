a42b754c755d44ff36edf6acf4651952d4cc55f5
Mark Mulder <markmulder@gmail.com>
2015-10-17 20:33:15 +0200

    Wip posting data

--- static/components/submit_button.js
+++ static/components/submit_button.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
+import fetch from 'isomorphic-fetch';
+
 // TODO: Find out best way to receive this from parent for better data flow
 import store from './../store';
 
 var SubmitButton = React.createClass({
   handleClick: function(event) {
-    console.log('Clicked submit button');
+    event.preventDefault();
 
     console.log('state at time of submit is: ', store.getState());
 
+    const { template, rules } = store.getState();
+
+    var id = 1;
+    var url = `http://localhost:3002/receive/${id}.json`;
+    fetch(url, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        notifier: {
+          template: template,
+          rules: rules,
+        }
+      })
+    })
+      .then(response => response.json()) // can be removed?
+      .then(json => console.log(json))
+      .catch(exception => console.log('POST failed:', exception));
   },
 
   render: function() {
