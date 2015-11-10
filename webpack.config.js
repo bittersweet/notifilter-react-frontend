8dc0bdcc70c1c4bbd33d65095ba6147a3d04d7f4
Mark Mulder <markmulder@gmail.com>
2015-11-10 13:51:02 +0100

    Indent and use single quotes in web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    context: __dirname + "/static",
-    entry: {
-        javascript: "./index.js",
-        html: "./index.html",
-    },
+  context: __dirname + '/static',
+  entry: {
+    javascript: './index.js',
+    html: './index.html',
+  },
 
-    output: {
-        filename: "index.js",
-        path: __dirname + "/dist",
-    },
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist',
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ["react-hot", "babel-loader"],
-            },
-            {
-                test: /\.html$/,
-                loader: "file?name=[name].[ext]",
-            }
-            ],
-        },
-    }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      }
+    ]
+  }
+}
